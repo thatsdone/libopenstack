e7f23d7daf970575e6a95acb382ea30eb1c49967
Masanori Itoh <masanori.itoh@gmail.com>
2014-04-29 02:31:46 +0900

    Add command line options (user, password, tenant, auth_url).

--- src/main.c
+++ src/main.c
@@ -17,26 +17,21 @@
  */
 
 
-//#include <json/json.h>
-//#include <stdio.h>
-//#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 //#include <sys/types.h>
 //#include <string.h>
+//#include <json/json.h>
 //#include <curl/curl.h>
-#include <stdlib.h>
 
 #include <openstack/keystone.h>
 #include <openstack/swift.h>
 
 
-extern void swift_stat(struct keystone *);
+int debug = 0;
 
-/*
-OS_AUTH_URL=http://10.10.1.77:5000/v2.0/
-OS_PASSWORD=13a3912e3b424c96
-OS_TENANT_NAME=admin
-OS_USERNAME=admin
- */
 int main (int argc, char **argv)
 {
 	char *username   = getenv("OS_USERNAME");
@@ -45,6 +40,49 @@ int main (int argc, char **argv)
 	char *auth_url   = getenv("OS_AUTH_URL");
 	
 	struct keystone *keystone;
+	int this_option_optind = optind ? optind : 1;
+	int opt_idx = 0;
+	static struct option long_options[] = {
+		{"os-username", required_argument, NULL, 0},
+		{"os-password", required_argument, NULL, 1},
+		{"os-tenant-name", required_argument, NULL, 2},
+		{"os-auth-url", required_argument, NULL, 3},
+		{"debug", required_argument, NULL, 4},
+		{0, 0, 0, 0}
+	};
+	int c;
+	while ((c = getopt_long(argc, argv, "u:p:t:a:d", long_options, &opt_idx)) != -1) {
+		switch (c) {
+		case '?':
+			exit(1);
+			break;
+		case 0:
+		case 'u':
+			username = optarg;
+			break;
+		case 1:
+		case 'p':
+			password = optarg;
+			break;
+		case 2:
+		case 't':
+			tenantname = optarg;
+			break;
+		case 3:
+		case 'a':
+			auth_url = optarg;
+			break;
+		case 4:
+		case 'd':
+			debug = 1;
+		}
+	}
+
+	if (username == NULL || tenantname == NULL || password == NULL ||
+		auth_url == NULL) {
+		printf("Specify username, tenantname, password and auth_url");
+		exit(1);
+	}
 
 	keystone = keystone_authenticate(username, tenantname, password,
 					 auth_url);
